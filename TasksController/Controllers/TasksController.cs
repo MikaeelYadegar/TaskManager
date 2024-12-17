441fde0efc33183688c5fc89b7c9943af5351f40
MikaeelYadegar <165888959+MikaeelYadegar@users.noreply.github.com>
2024-12-17 10:05:26 +0330

    Add files via upload

---
+++ TasksController/Controllers/TasksController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TasksController.Models;
+namespace TasksController.Controllers
+{
+    public class TasksController : Controller
+    {
+        // GET: Tasks
+        public ActionResult Index()
+        {
+            var tasks = Session["Tasks"] as List<Task>;
+            if (tasks == null)
+            {
+                tasks = new List<Task>();
+            }
+            return View(tasks);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Task task)
+        {
+            task.CreatedAt = DateTime.Now;
+            var tasks = Session["Tasks"] as List<Task>;
+            if (tasks == null)
+            {
+                tasks = new List<Task>();
+            }
+            tasks.Add(task);
+            Session["Tasks"] = tasks;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult CreatePer()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreatePer(Task task)
+        {
+            task.CreatedAt = DateTime.Now;
+            var tasks = Session["Tasks"] as List<Task>;
+            if (tasks == null)
+            {
+                tasks = new List<Task>();
+            }
+            tasks.Add(task);
+            Session["Tasks"] = tasks;
+            return RedirectToAction("Index");
+        }
+
+
+
+
+    }
+}
---
+++ TasksController/Models/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TasksController.Models
+{
+    public enum Priority
+    {
+        Urgent,
+        NotUrgent
+    }
+    public class Task
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Priority TaskPriority { get; set; }
+
+        public string TitlePer { get; set; }
+        public string DescriptionPer { get; set; }
+        public bool IsCompletedPer { get; set; }
+        public DateTime CreatedAtPer { get; set; }
+        public Priority TaskPriorityPer { get; set; }
+    }
+}
